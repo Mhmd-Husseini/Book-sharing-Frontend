f4559474ce2ef46f115848e5ae0e29583b3f073e
Husseini <mhmd.husseinyy@gmail.com>
2023-08-25 01:21:32 +0300

    adding authentication page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './styles/App.css';
+import './styles/utilities.css'
+import './styles/utilities2.css'
+import Authentication from './components/pages/Authentication';
+import RecipeDetails from './components/pages/RecipeDetails';
+import Landing from './components/pages/Landing';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Authentication />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='/landing/:recipe_id' element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
