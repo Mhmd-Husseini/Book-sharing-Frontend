471ba08301535ee722889ca62defb099c07e5c9e
Husseini <mhmd.husseinyy@gmail.com>
2023-08-25 08:15:07 +0300

    creating logout function

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import "./style.css";
-const Nav = ({handleOpenModal, handleOpenCalendarModal}) => {
-    return (
-        <>
-        <nav className="navbar">
-            <div className="navbar-brand">Book Share</div>
-            <ul className="navbar-list">
-                <li className="navbar-item" onClick={handleOpenModal}>Add Book</li>
-                <li className="navbar-item">Logout</li>
-            </ul>
-        </nav>
-        </>
-    );
+import { useNavigate } from "react-router-dom"; 
+
+const Nav = ({ handleOpenModal, handleOpenCalendarModal }) => {
+  const navigate = useNavigate(); 
+
+  const handleLogout = () => {
+    localStorage.clear();
+
+    navigate("/"); 
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">Books Hub</div>
+        <ul className="navbar-list">
+          <li className="navbar-item" onClick={handleOpenModal}>
+            Add Book
+          </li>
+          <li className="navbar-item" onClick={handleLogout}>
+            Logout
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
+
 export default Nav;
